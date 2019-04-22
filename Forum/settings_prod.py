70bde0500c6ab4a21e05479fff2835d6b40f0996
Alex W <runthesun@gmail.com>
2019-04-22 15:01:13 -0700

    prod commit w/ EC2 updated info

--- Forum/settings_prod.py
+++ Forum/settings_prod.py
@@ -26,7 +26,7 @@ with open('secret.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.14.65.7"]
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
